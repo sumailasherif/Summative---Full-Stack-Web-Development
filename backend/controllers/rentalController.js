7fbec96d05d1cd132091adcfe756efcbf0778318
Sherif <sumailasherif773@gmail.com>
2026-09-20 20:00:15 +0400

    GET added

--- backend/controllers/rentalController.js
+++ backend/controllers/rentalController.js
@@ -2,3 +2,12 @@ const rentalModel = require('../models/rentalModel');
 const customerModel = require('../models/customerModel');
 const vehicleModel = require('../models/vehicleModel');
 
+// GET /api/rentals
+async function getAllRentals(req, res) {
+  try {
+    const rentals = await rentalModel.getAllRentals();
+    res.status(200).json(rentals);
+  } catch (error) {
+    res.status(500).json({ message: 'Error retrieving rentals', error: error.message });
+  }
+}
